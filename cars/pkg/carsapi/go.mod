13e0680b8a0d7175a8d9f9358406c29188c4b055
LivePurposefully <livepurposefully@LivePurullysMBP.fios-router.home>
2021-05-12 18:49:37 -0400

    extract get function into carsapi package, and use it for the get route

---
+++ cars/pkg/carsapi/go.mod
@@ -0,0 +1,3 @@
+module github.com/LivePurposefully/cars-refactor/carsapi
+
+go 1.16
--- cars/src/main.go
+++ cars/src/main.go
@@ -12,6 +12,7 @@ import (
 	"github.com/gin-contrib/cors"
 
 	"github.com/LivePurposefully/cars-refactor/cars/pkg/models"
+	"github.com/LivePurposefully/cars-refactor/cars/pkg/carsapi"
 )
 
 
@@ -68,45 +69,7 @@ func main() {
 	}
 
 	//GET '/'  --> all cars
-	router.GET("/cars", func(c *gin.Context) {
-		results := make([]models.Vehicle, 0)
-
-		var rows *sql.Rows
-		var err error
-
-		rows, err = db.Query(SelectAllCarsQuery)
-		if err != nil {
-			log.Println(err)
-		}
-		defer rows.Close()
-
-		// count of records returned
-		count := 0
-
-		for rows.Next() {
-			var obj models.Vehicle
-
-			err = rows.Scan(&obj.Id, &obj.Make, &obj.Model, &obj.Year)
-
-			// if the scan was successful, load the row
-			if err == nil {
-				results = append(results, obj)
-				count++
-			}
-		}
-
-		// show count of successfully processed rows
-		log.Println("Rows returned: " + strconv.Itoa(count))
-
-		if err = rows.Err(); err != nil {
-			// Abnormal termination of the rows loop
-			// close should be called automatically in this case
-			log.Println(err)
-		}
-
-		c.JSON(200, results)
-		//c.JSON(200, storeData)
-	})
+	router.GET("/cars", carsapi.Index(c *gin.Context))
 
 	//POST '/cars'  --> create cars
 	router.POST("/cars", func(c *gin.Context) {
